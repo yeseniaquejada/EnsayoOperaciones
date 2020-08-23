c4a8f3cb6e03b84b8cdd9497c18729f553130ae2
yesenia111 <yesenia.quejadarojas@medellin.edu.co>
2020-08-22 20:13:31 -0500

    Ensayo de operaciones

---
+++ operaciones.js
@@ -0,0 +1,26 @@
+var n1=9, n2=3, resultado;
+
+function imprimir(){
+    document.getElementById("1").innerHTML=n1+n2
+    document.getElementById("2").innerHTML=n1-n2
+    document.getElementById("3").innerHTML=n1*n2
+    document.getElementById("4").innerHTML=n1**n2
+    document.getElementById("5").innerHTML=n1/n2
+    document.getElementById("6").innerHTML=n1%n2
+    document.getElementById("7").innerHTML=++n1
+    document.getElementById("8").innerHTML=--n2
+    document.getElementById("9").innerHTML=n1=n2
+    document.getElementById("10").innerHTML=n1+=n2
+    document.getElementById("11").innerHTML=n1-=n2
+    document.getElementById("12").innerHTML=n1*=n2
+    document.getElementById("13").innerHTML=n1/=n2
+    document.getElementById("14").innerHTML=n1%=n2
+    document.getElementById("15").innerHTML=n1**=n2
+    document.getElementById("16").innerHTML=n1==n2
+    document.getElementById("17").innerHTML=n1!=n2
+    document.getElementById("18").innerHTML=n1!==n2
+    document.getElementById("19").innerHTML=n1>n2
+    document.getElementById("20").innerHTML=n1<n2
+    document.getElementById("21").innerHTML=n1>=n2
+    document.getElementById("22").innerHTML=n1<=n2
+}
